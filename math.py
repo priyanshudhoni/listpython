88ba7201cac2ec4b626c03cda1d0660f4445d0ca
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-15 18:10:16 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#operators
+from math import*
+a=12
+b=6
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print(pow(a,b))
+print(divmod(a,b))
+print(ceil(12.2))
+print(floor(13.6))
+print(round(12.5))
+
