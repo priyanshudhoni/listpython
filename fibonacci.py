ec92a4ba81bb538a2f70cc36c052d5cd9c36dd79
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-15 18:01:52 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the number of fibonacci terms"))
+i=0
+a1=0
+b=1 
+i=i+2
+print(a1,end=" ")
+print(b,end=" ")
+while(i<a):
+    c=a1+b
+    print(c,end=" ")
+    b,a1=c,b
+    
+    i=i+1
+
+    
