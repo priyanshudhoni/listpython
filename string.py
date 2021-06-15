1c01306a366f90a1c39fca86906a6fc275444208
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-15 17:50:45 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,19 @@
+a="Virat is the best player"
+print(a.lower())
+print(a.upper())
+print(a[::-1])
+print(a[2:4])
+print(a[-3::])
+print(a.count('i'))
+print(a.title())
+print(a.capitalize())
+print(a.split())
+# convert a list to string 
+b=["India","My","Country"]
+b=" ".join(b)
+print(b)
+print(a.isalpha())
+print(a.isalnum())
+print(a.isupper())
+print(a.endswith("player"))
+print(a.startswith("V"))
