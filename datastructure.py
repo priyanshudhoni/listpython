50b0065aedd0618bcee7a302ecf89cea58d70f90
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-16 17:20:07 +0530

    Update datastructure.py

--- datastructure.py
+++ datastructure.py
@@ -12,3 +12,30 @@ b=""
 for h in reversed(a):
     b=b+h 
 print(b)
+s=(1,8,3)
+d=sorted(s)
+d[0]=10
+print(d)
+from numpy import *
+a=array([2,4,1])
+a.sort()
+print(a)
+s=tuple(d)
+print(s)
+f=[4,8,9]
+q=list(enumerate(s))
+print(q)
+
+q=[[2,5],5,6]
+e=[]
+e=q;
+
+print(e)
+print(id(q))
+print(id(e))
+e=q.copy()
+print(id(q))
+print(id(e))
+q[0][0]=14
+print(e)
+
